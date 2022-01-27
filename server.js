0eb3790c2b95075d2ddd7d4c73c9cae0c067cbf4
Michael Klein <Michaelklein1997@gmail.com>
2022-01-27 17:43:50 -0600

    GET fetch now is operational

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')))
 
+app.get('/api/notes', (req, res) => res.json(notes))
+
 app.post('/api/notes', (req, res) => {
     console.info(`${req.method} request recieved to add a note`)
     const { title, text } = req.body;
